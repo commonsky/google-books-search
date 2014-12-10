7cf23edebe4211d684e5615961ee457e18d55029
jongrover <hello@jonathangrover.com>
2014-12-10 12:09:08 -0500

    cleanup code solution, add css

--- js/app.js
+++ js/app.js
@@ -1,59 +1,35 @@
 $(function(){
-
-  //user clicks submit
+  // user types book search term
+  // user clicks submit
   $('#search').click(function(event){
     event.preventDefault();
 
+    // take user data and format it into api request url
+    var term = $('#term').val();
+    var apiURL = 'https://www.googleapis.com/books/v1/volumes?q='+term;
 
-    //take user data and format it into api request url
-    var book = $('#book').val();
-    var searchUrl = 'https://www.googleapis.com/books/v1/volumes?q='+book;
-
-    //make an ajax request
+    // make an ajax request
     $.ajax({
-      url: searchUrl,
+      url: apiURL, 
       type: 'GET',
       dataType: 'json',
-      //if success then construct books from json and append them into results section element.
+      // if success then construct books from json and append them into results section element.
       success: function(response) {
         //console.log(response.items);
 
         $.each(response.items, function(i, book){
-          
-          //get each books info
-          $.ajax({
-              url: book.selfLink,
-              type: 'GET',
-              dataType: 'json',
-              success: function(response){
-                //response.volumeInfo.imageLinks.thumbnail
-                //console.log(response.volumeInfo.imageLinks.thumbnail);
-                var bookEle = '<div class="book"><img src="'+response.volumeInfo.imageLinks.thumbnail+'" alt="'+response.volumeInfo.title+'"></div>';
-
-                $('#results').append(bookEle);
-
-              },
-              error: function(jqXHR, textStatus, error) {
-                console.log(jqXHR);
-                console.log(textStatus);
-                console.log(error);
-              }
-          });
-
+          var bookEle = '<div class="book"><a href="'+book.volumeInfo.previewLink+'"><img src="'+book.volumeInfo.imageLinks.thumbnail+'" alt="'+book.volumeInfo.title+'"><h2>'+book.volumeInfo.title+'</h2><h3>'+book.volumeInfo.authors+'</h3></a></div>';
+          $('#results').append(bookEle);
         });
 
       },
-      //if error log the error
+      // if error log the error
       error: function(jqXHR, textStatus, error) {
         console.log(jqXHR);
         console.log(textStatus);
         console.log(error);
       }
     });
-    //$.get();
-    //$.post();
-    //$.getJSON();
       
   });
-
 });
